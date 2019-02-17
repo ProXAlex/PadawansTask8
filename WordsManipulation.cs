ecb92678b60c53580f5a757f99086eeaac8272b4
Alex <alex_goreloshev@mail.ru>
2019-02-17 12:12:52 +0300

    implement RemoveDuplicateWords

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
 using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace PadawansTask8
 {
@@ -7,8 +9,33 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (text == null)
+                throw new ArgumentNullException("Argument cannot be null");
+            var split = text.Split(new char[] { '.', ',', '!', '?', '-', ':', ';', ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+
+            Dictionary<string, int> resultDictionary = new Dictionary<string, int>();
+            foreach (var s in split)
+            {
+                if (!resultDictionary.ContainsKey(s))
+                    resultDictionary.Add(s, 0);
+                else
+                    resultDictionary[s]++;
+            }
+
+            foreach (KeyValuePair<string, int> item in resultDictionary)
+            {
+                if (item.Value == 0)
+                    continue;
+                for (int i = 1; i <= item.Value; i++)
+                {
+                    int index = text.LastIndexOf(item.Key);
+                    text = text.Remove(index, item.Key.Length);
+
+                }
+            }
+
         }
     }
 }
